247ea3bc4f41baf637f4f2a91a61e7a590e97fe7
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-04-25 15:18:31 -0600

    Se crea la funcion open_image

--- Practica_7.py
+++ Practica_7.py
@@ -4,3 +4,10 @@ from PIL import Image, ImageTk
 import threading
 import time
 import random
+
+
+def open_image(name, size_1, size_2):
+    image = Image.open(name)
+    new_image = image.resize((size_1, size_2))
+    render = ImageTk.PhotoImage(new_image)
+    return render
