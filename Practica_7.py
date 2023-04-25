4d7446012d946cf7b10910d3cbb6c195eb40b6d3
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-04-25 15:19:26 -0600

    Se crea la ventana

--- Practica_7.py
+++ Practica_7.py
@@ -11,3 +11,16 @@ def open_image(name, size_1, size_2):
     new_image = image.resize((size_1, size_2))
     render = ImageTk.PhotoImage(new_image)
     return render
+
+
+if __name__ == "__main__":
+    window = Tk()
+    window.title("Practica 7")
+    window.geometry("600x400")
+
+    imag = open_image("Estacionamiento.png", 600, 400)
+    lbl = Label(window, image=imag).pack()
+
+    imag_2 = open_image("Vehiculo.png", 80, 150)
+
+    window.mainloop()
