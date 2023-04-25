5da64ecb345324ec87615d5ba46232e8398d46d1
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-04-25 15:25:01 -0600

    Se crea una nueva ventana para seleccionar el tiempo

--- Practica_7.py
+++ Practica_7.py
@@ -18,7 +18,7 @@ class Producer_Consumer:
         menu_bar = Menu(self.window)
         menu_producer_consumer = Menu(menu_bar)
         menu_producer_consumer.add_command(
-            label='producto-consumidor')
+            label='producto-consumidor',command=self.Edit)
         menu_bar.add_cascade(label="Editar", menu=menu_producer_consumer)
         self.window.config(menu=menu_bar)
 
@@ -31,6 +31,36 @@ class Producer_Consumer:
                 return float(self.time_producer)
             if self.time_consumer != "Aleatorio" and producer_or_consumer == "consumer":
                 return float(self.time_consumer)
+    
+    def edit_time(self):
+        self.time_consumer = self.cmb_time_consumer.get()
+        self.time_producer = self.cmb_time_producer.get()
+            
+    def Edit(self):
+        root = Tk()
+        root.title("Edit")
+        root.geometry("180x180")
+
+        Label(root, text="Productor").place(x=10, y=10)
+        variable_producer = StringVar()
+        self.cmb_time_producer = ttk.Combobox(
+            root, width=17, textvariable=variable_producer)
+        self.cmb_time_producer["values"] = ("Aleatorio", "0.5", "1", "2")
+        self.cmb_time_producer.place(x=10, y=40)
+        self.cmb_time_producer.current(0)
+
+        Label(root, text="Consumidor").place(x=10, y=80)
+        variable_consumer = StringVar()
+        self.cmb_time_consumer = ttk.Combobox(
+            root, width=17, textvariable=variable_consumer)
+        self.cmb_time_consumer["values"] = ("Aleatorio", "0.5", "1", "2")
+        self.cmb_time_consumer.place(x=10, y=110)
+        self.cmb_time_consumer.current(0)
+
+        btn_save = Button(root, text="Guardar", command=self.edit_time)
+        btn_save.place(x=20, y=150)
+
+        root.mainloop()
 
 def open_image(name, size_1, size_2):
     image = Image.open(name)
